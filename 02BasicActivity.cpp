a733a7991054f3dc14f3bfba5cca506569f86ada
Rhona David <rhonadavid25@gmail.com>
2024-10-08 09:53:55 +0800

    Add files via upload

---
+++ 02BasicActivity.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION";
+	cout << "COLLEGE OF COMPUTER STUDIES";
+	cout << "BSIT 1-A";
+	cout << "RHONA S. DAVID";
+	cout << "P-9, BATOBALANI PARACALE CAMARINES NORTE";
+	
+	return 0;
+}
+	
+	
---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name, course, school;
+	
+	cout << "Nickname:";
+	cin >> name;
+	cout << "Course-Year Level:";
+	cin >> course;
+	cout << "School:";
+	cin >> school;
+	return 0;
+}
