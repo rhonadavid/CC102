6badb413a2717005a31e5098b379f0753fe87bf9
Rhona David <rhonadavid25@gmail.com>
2024-10-15 10:08:53 +0800

    Add files via upload

---
+++ Midterm Hands-on Exam.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+
+	float sugarPriceUSD = 0, ricePricePound = 0, sardinadsPricePound = 0, coffeePricePound = 0, milkPriceUSD = 0;
+	int sugarQty, riceQty, sardinasQty, coffeeQtyt, milkQty, 
+	int sugarCostPHP, riceCostPHP, sardinasCostPHP, coffeeCostPHP, milkCostPHP,
+	
+	cout << "sugarPriceUSD" << endl;
+	cin >> sugarPrice;
+	cout << "ricePriceUSD" << endl;
+	cin >> ricePrice;
+	cout << "sardinasPriceUSD" << endl;
+	cin >> sardinasPrice;
+	cout << "coffeePriceUSD" << endl;
+	cin >> coffeePrice;
+	cout << "milkPriceUSD" << endl;
+	cin >> milkPrice;
+	
+	cout << "sugarQty" << endl;
+	cin >> sugarQty;
+	cout << "riceQty" << endl;
+	cin >> ricePriceQty;
+	cout << "sardinasQty" << endl;
+	cin << sardinasQty;
+	cout << "coffee" << endl;
+	cin << coffee;
+	cout << "milkty" << endl;
+	
+	return 0;
+	
+}
+	
