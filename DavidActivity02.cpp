f0535c18333da826eb07382c794f6af6e024df6a
Rhona David <rhonadavid25@gmail.com>
2024-11-13 09:15:03 +0800

    Add files via upload

---
+++ DavidActivity02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+	int age;
+	cout << "what is your age";
+	cin >> age;
+	
+	if (age < 16){
+		cout << "Too young to drive" << endl;
+	}
+	else if (age ==16) {
+		cout << "Better clear the road" << endl;
+	}
+	else{
+			cout << " You are getting pretty old" << endl;
+	}
+		return 0;
+}
